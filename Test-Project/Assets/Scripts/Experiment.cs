6aaf2b766f93fa9c876b84c229b8bbd42359b48d
Smith <taylor.n.smith@vanderbilt.edu>
2019-07-12 14:00:58 -0500

    All Avatars Added and Experiment Almost Finished

--- Test-Project/Assets/Scripts/Experiment.cs
+++ Test-Project/Assets/Scripts/Experiment.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bfd179ab0680667da033205a67fc4feb50eff734e676e4fc553fcc898d283780
-size 5630
+oid sha256:a1d62e315552d21e2ad0a641e3e4189b3084edcc5ac74460623cfd06d54c81ae
+size 8236
