367d16e72c7d9b46bf0e940af4a7a673ede595ca
Smith <taylor.n.smith@vanderbilt.edu>
2019-07-16 14:14:15 -0500

    CSV Integration Added

--- Test-Project/Assets/Scripts/Experiment.cs
+++ Test-Project/Assets/Scripts/Experiment.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a1d62e315552d21e2ad0a641e3e4189b3084edcc5ac74460623cfd06d54c81ae
-size 8236
+oid sha256:1931b07161ffbf182ee35b149b2a1f71c41ef52264415ae9272c287ebe10f5ce
+size 10353
