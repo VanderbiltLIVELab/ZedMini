fe00165056b5f0e7409a936ff2b6d805a4161d31
Irisa Myint <irisa.t.myint@vanderbilt.edu>
2019-08-02 13:35:14 -0500

    Issues Fixed and Protocol Changed

--- Zed/Assets/Scripts/Experiment.cs
+++ Zed/Assets/Scripts/Experiment.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aa0cd7a0e375b21c38a2bab62f966f45050887e386b60d289b7287a9be3c2672
-size 11350
+oid sha256:7e08f8f866b8ae87431d281f00f464c304c4d8d53a6de1e1e0a6eb8cad7f2a15
+size 11780
