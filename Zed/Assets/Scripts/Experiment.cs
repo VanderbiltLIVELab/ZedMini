877cb324b796eae8c72a88ab0b4ed93b5b6fc0c4
Irisa Myint <irisa.t.myint@vanderbilt.edu>
2019-08-06 13:32:13 -0500

    Experimental Procedure Finalized and Green Screen Orientation Changed

--- Zed/Assets/Scripts/Experiment.cs
+++ Zed/Assets/Scripts/Experiment.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:637a651ad4fdb9b56d84946b7f41012b52abbe0cdebab45e15d171377a2744d3
-size 13501
+oid sha256:bc3f61e6e47a8562629f45e7effd22d4f33ff5462ce2e015bb2ff80d0142492e
+size 14087
--- Zed/csvParseFile.py
+++ Zed/csvParseFile.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:716f19d83606aed1573999acba2c2b5f20b7c58e6c3156dfd18df9dc615be52d
-size 1869
+oid sha256:a717de56d6d1aeedf412bf249d3bea5b294907d5066474f81ef0f1992cfb7f93
+size 1841
